169b482d3b80673d62c81fac2a55fd855682428e
cfmorris <clifford.morris.0844@gmail.com>
2022-04-04 14:37:35 -0400

    Update comments

--- pascals-diagnol.py
+++ pascals-diagnol.py
@@ -1,21 +1,22 @@
 # Author:cfmorris
 # Source: Codewars 
 # Title: Pascals Triangle.
-# Challenge: Generate the (n)th diagnol of Pascal's triangle to the (l)th position
+# Challenge: Generate the (n)th diagnol of Pascal's triangle to the (l)th position.
+# https://www.codewars.com/kata/576b072359b1161a7b000a17
 # https://en.wikipedia.org/wiki/Pascal%27s_triangle
 
 
-# This solution leverages the fact that the lth value of the nth diagnol can be calculated 
-# by taking the sum all values [0:l] in the (n-1) diagnol.
+# This solution leverages the fact that the lth value of the nth diagnol can be 
+# calculated by taking the sum all values [0:l] in the (n-1) diagnol.
 
 def generate_diagonal(n, l):
     diagnol=[]
-    for i in range(l): #first loop builds a list of all 1s
+    for i in range(l):  #first loop builds a list of all 1s
         diagnol.append(1)
     if n==0:                
-        return diagnol[0:l]  #added an early return for the n=0 fringe case
+        return diagnol[0:l]  
     
-    else:
+    else:               #second loop iterates over previous loops.
         for item in range(n):
             nextdiag=[]
             for i in range(l+n):
